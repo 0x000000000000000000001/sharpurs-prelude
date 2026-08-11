196a2ccd6dd20fc872655740c19f96ae1f79b9c3
0x1 <kevin.francart3@gmail.com>
2026-08-11 12:55:51 +0200

    misc

--- src/Data/Show.fs
+++ src/Data/Show.fs
@@ -2,4 +2,12 @@ let showIntImpl (x: obj) : obj = box (string (unbox<int> x))
 let showNumberImpl (x: obj) : obj = box (string (unbox<float> x))
 let showStringImpl (x: obj) : obj = box ("\"" + unbox<string> x + "\"")
 let showCharImpl c = string (unbox<char> c)
-let showArrayImpl a = undefined
+let showArrayImpl (f: obj) (xs: obj) =
+    let xs' = unbox<obj[]> xs
+    let mutable res = "["
+    for i = 0 to xs'.Length - 1 do
+        res <- res + unbox<string> (Sharpurs_Prelude.sharpurs_apply f xs'.[i])
+        if i < xs'.Length - 1 then
+            res <- res + ","
+    res <- res + "]"
+    box res
