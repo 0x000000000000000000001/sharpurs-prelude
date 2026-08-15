cf7cf9ed7276564cd028868c7402dd5e57b55618
0x1 <kevin.francart3@gmail.com>
2026-08-15 23:04:32 +0200

    misc

--- src/Control/Apply.fs
+++ src/Control/Apply.fs
@@ -1,1 +1,13 @@
-let arrayApply = undefined
+let arrayApply (fs: obj) (xs: obj) : obj =
+    let fsArr = unbox<obj[]> fs
+    let xsArr = unbox<obj[]> xs
+    let lenF = fsArr.Length
+    let lenX = xsArr.Length
+    let result = Array.zeroCreate (lenF * lenX)
+    let mutable n = 0
+    for i = 0 to lenF - 1 do
+        let f = fsArr.[i]
+        for j = 0 to lenX - 1 do
+            result.[n] <- sharpurs_apply f xsArr.[j]
+            n <- n + 1
+    result :> obj
--- src/Data/EuclideanRing.fs
+++ src/Data/EuclideanRing.fs
@@ -1,10 +1,23 @@
-let intDiv a b = (unbox<int> a) / (unbox<int> b)
-let intDegree a = System.Math.Abs(unbox<int> a)
+let absInt (n: int) =
+    if n = System.Int32.MinValue then System.Int32.MaxValue
+    else System.Math.Abs(n)
+
+let intDiv a b = 
+    let x = unbox<int> a
+    let y = unbox<int> b
+    if y = 0 then 0
+    else
+        let yy = absInt y
+        let m = ((x % yy) + yy) % yy
+        if y = -1 && x = System.Int32.MinValue then System.Int32.MinValue
+        else ((x - m) / y)
+        
+let intDegree a = absInt (unbox<int> a)
 let numDiv a b = (unbox<float> a) / (unbox<float> b)
 let intMod a b =
     let x = unbox<int> a
     let y = unbox<int> b
     if y = 0 then 0
     else
-        let yy = System.Math.Abs(y)
+        let yy = absInt y
         ((x % yy) + yy) % yy
