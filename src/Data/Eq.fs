4fcb2031c0a30fbc740de8d44bf1939257a91329
0x1 <kevin.francart3@gmail.com>
2026-08-16 13:50:46 +0200

    misc

--- src/Data/Eq.fs
+++ src/Data/Eq.fs
@@ -6,14 +6,11 @@ let eqBooleanImpl (a: obj) (b: obj) = unbox<bool> a = unbox<bool> b
 let eqArrayImpl (f: obj) (xs: obj) (ys: obj) =
     let xs' = unbox<obj[]> xs
     let ys' = unbox<obj[]> ys
-    printfn "eqArrayImpl: xs.Length=%d, ys.Length=%d" xs'.Length ys'.Length
     if xs'.Length <> ys'.Length then box false
     else
         let mutable eq = true
         for i = 0 to xs'.Length - 1 do
             let isEq = unbox<bool> (sharpurs_apply (sharpurs_apply f xs'.[i]) ys'.[i])
-            printfn "eqArrayImpl: xs[%d]=%A, ys[%d]=%A, isEq=%b" i xs'.[i] i ys'.[i] isEq
             if not isEq then
                 eq <- false
-        printfn "eqArrayImpl: eq=%b" eq
         box eq
